87571ae034f79658ce6c3390f58105a67bb24088
ysmny <whateveriloveid@gmail.com>
2023-08-28 16:29:35 +0700

    added seeder
    Menambah data seeder

--- database/seeders/MajorSeeder.php
+++ database/seeders/MajorSeeder.php
@@ -13,6 +13,58 @@ class MajorSeeder extends Seeder
      */
     public function run(): void
     {
-        Major::factory()->count(15)->create();
+        Major::insert([
+            'major_name' => 'Administrasi Perkantoran',
+            'major_code' => 'MJ001',
+            'major_alias' => 'AP',
+        ], [
+            'major_name' => 'Akutansi',
+            'major_code' => 'MJ002',
+            'major_alias' => 'AK',
+        ], [
+            'major_name' => 'Farmasi',
+            'major_code' => 'MJ003',
+            'major_alias' => 'FR',
+        ], [
+            'major_name' => 'Keperawatan',
+            'major_code' => 'MJ004',
+            'major_alias' => 'KP',
+        ], [
+            'major_name' => 'Multimedia',
+            'major_code' => 'MJ005',
+            'major_alias' => 'MM',
+        ], [
+            'major_name' => 'Pemasaran',
+            'major_code' => 'MJ006',
+            'major_alias' => 'PM',
+        ], [
+            'major_name' => 'Perhotelan',
+            'major_code' => 'MJ007',
+            'major_alias' => 'PH',
+        ], [
+            'major_name' => 'Tata Boga',
+            'major_code' => 'MJ008',
+            'major_alias' => 'TB',
+        ], [
+            'major_name' => 'Rekayasa Perangkat Lunak',
+            'major_code' => 'MJ009',
+            'major_alias' => 'RPL',
+        ], [
+            'major_name' => 'Tata Busana',
+            'major_code' => 'MJ010',
+            'major_alias' => 'TBA',
+        ], [
+            'major_name' => 'Keuangan Syariah',
+            'major_code' => 'MJ011',
+            'major_alias' => 'KSY',
+        ], [
+            'major_name' => 'Tata Rias',
+            'major_code' => 'MJ012',
+            'major_alias' => 'TRS',
+        ], [
+            'major_name' => 'Teknik Komputer Jaringan',
+            'major_code' => 'MJ013',
+            'major_alias' => 'TKJ',
+        ]);
     }
 }
